bb2af43dd0bef2b594b3099d48fd3365abe83f3c
RedOren28 <clem.chevrier428@gmail.com>
2023-05-15 21:05:32 +0200

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,8 +3,13 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Poil;
+use App\Entity\Race;
 use App\Entity\User;
+use App\Entity\Espece;
+use App\Entity\Regime;
 use App\Entity\Couleur;
+use App\Entity\Alimentation;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -25,6 +30,12 @@ class AppFixtures extends Fixture
 
         // Liste des choses à ajouter
         $couleurs = array('acajou','arlequin','aubère','belton','sésame','sésame rouge','sésame noir','bicolore','blanc','bleu','blenheim','bigaré','bringé','caille','cannelle','cerf','charbonné','châtain','pluricolore','rouanné','tacheté','tiqueté','zain','chevreuil','chocolat','citron','crème','écaille de tortue','faon','fauve','foie','gris','isabelle','puce','lilas','louvet','marron','merle','moucheté','multicolore','noir','noir et feu','roux','rubis','sable','tigré','alezan','appaloosa','bai','palomino','pie','rouan','souris','blond','papillon','ombré','panaché','pie','particolore','parsemé','pie noir','pie rouge');
+        $poils = array('acajou','arlequin','aubère','belton');
+        $regimes = array('acajou','arlequin','aubère','belton');
+        $alimentations = array('acajou','arlequin','aubère','belton');
+        $especes = array('acajou','arlequin','aubère','belton');
+        $races = array('acajou','arlequin','aubère','belton');
+
 
         // Admin
         $admin = new User;
@@ -46,6 +57,46 @@ class AppFixtures extends Fixture
             $uneCouleur->setNom($couleur);
 
             $manager->persist($uneCouleur);
+        }
+
+        // Poil
+        foreach ($poils as $poil) {
+            $unpoil = new Poil();
+            $unpoil->setType($poil);
+
+            $manager->persist($unpoil);
+        }
+
+        // Régime
+        foreach ($regimes as $regime) {
+            $unRegime = new Regime();
+            $unRegime->setNom($regime);
+
+            $manager->persist($unRegime);
+        }
+
+        // Alimentation
+        foreach ($alimentations as $alimentation) {
+            $uneAlimentation = new Alimentation();
+            $uneAlimentation->setNom($alimentation);
+
+            $manager->persist($uneCouleur);
+        }
+
+        // Espèce
+        foreach ($especes as $espece) {
+            $uneEspece = new Espece();
+            $uneEspece->setNom($espece);
+
+            $manager->persist($uneEspece);
+        }
+
+        // Race
+        foreach ($races as $race) {
+            $uneRace = new Race();
+            $uneRace->setNom($race);
+
+            $manager->persist($uneRace);
         }
 
         $manager->flush();
